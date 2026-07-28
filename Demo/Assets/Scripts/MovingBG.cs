707e329f3bbf1bf511a4a239937ab1df8fa09fe4
zekee0010-wq <zekee0010@gmail.com>
2026-07-28 14:48:02 -0700

    "Don't you have a human heart?"

--- Demo/Assets/Scripts/MovingBG.cs
+++ Demo/Assets/Scripts/MovingBG.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingBG : MonoBehaviour
-{
+{ 
+    public int speed;
+    public float boundary;
+    public GameObject bg;
+    private Vector3 startingPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPos = transform.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(new Vector3(.001f, 0, 0));
+        transform.Translate(new Vector3(speed, 0, 0));
+
+        if(gameObject.transform.position.x >= boundary) {
+            Instantiate(bg, startingPos, Quaternion.identity);
+            Debug.Log("Called");
+            Destroy(gameObject);
+        }
     }
 }
