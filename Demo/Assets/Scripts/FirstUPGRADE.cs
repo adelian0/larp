06c4e3a6cbdb84af60aa14e8c2fb3d3c85d210ae
derek1-design <mcdreamyoc@gmail.com>
2026-07-30 09:11:44 -0700

    Update FirstUPGRADE.cs

--- Demo/Assets/Scripts/FirstUPGRADE.cs
+++ Demo/Assets/Scripts/FirstUPGRADE.cs
@@ -1,29 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FirstUPGRADE : MonoBehaviour
 {
     [Header("Components")]
-    public TMP_text priceText; 
-    public TMP_text incomeInfoText; 
+    public TMP_Text priceText;
+    public TMP_Text incomeInfoText;
 
     public int startPrice = 15;
-    public float upgradePriceMultiplier;
-    public float browniesPerUpgrade 0.1f ;
- 
+    public float upgradePriceMultiplier = 1.15f;
+    public float browniesPerUpgrade = 0.1f;
 
     int level = 0;
-{
-   priceText.text = CalculatePrice(). ToString();
-   incomeInfoText.text = level.ToString() + "x" + browniesPerUpgrade + "/s";
-   // 5 x 0.5/s
-}
-    int CalculatePrice(){
-        int Price = Mathf.RoundToInt(startPrice * Mathf.Pow(upgradePriceMultiplier,level));
+
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        if (priceText != null)
+            priceText.text = CalculatePrice().ToString();
+
+        if (incomeInfoText != null)
+            incomeInfoText.text = level.ToString() + "x" + browniesPerUpgrade + "/s";
+    }
+
+    int CalculatePrice()
+    {
+        int price = Mathf.RoundToInt(startPrice * Mathf.Pow(upgradePriceMultiplier, level));
         return price;
     }
-     public float CalculateIncomePerSecond(){
-        return browniesPerUpgrade = level; 
-     }
+
+    public float CalculateIncomePerSecond()
+    {
+        return level * browniesPerUpgrade;
+    }
 }
